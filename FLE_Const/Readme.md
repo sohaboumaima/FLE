c5bd4fefb622afdb8f67f8e6f94da6399d9ca603
sohaboumaima <ous219@lehigh.edu>
2023-09-27 21:09:48 -0400

    Removed experiments

--- FLE_Const/Readme.md
+++
@@ -1,2 +0,0 @@
-This is the code for the Full-Low Evaluation Methods For Bound and Linearly Constrained
-Derivative-Free Optimization
--- FLE_Const/project_on_feasible.m
+++
@@ -1,9 +0,0 @@
-function xproj = project_on_feasible(xtemp, pr)
-
-n = length(xtemp);
-H = eye(n);
-f = - xtemp;
-xproj =  quadprog(H,f,pr.Aineq,pr.ubineq,pr.Aeq,pr.beq,pr.lb,pr.ub);
-
-
-
